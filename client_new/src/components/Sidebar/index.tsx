e7478ccab9de00399340c88fedce5843b7528351
Akshat Jain <akshat22j@gmail.com>
2025-01-23 14:23:06 +0530

    Image URL changes

--- client_new/src/components/Sidebar/index.tsx
+++ client_new/src/components/Sidebar/index.tsx
@@ -58,7 +58,7 @@ const Sidebar = () => {
         </div>
         {/* Team */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700 ">
-          <Image src="/logo.png" width={40} height={40} alt="team" />
+          <Image src="https://pm-s3-bucket-image.s3.us-east-1.amazonaws.com/logo.png" width={40} height={40} alt="team" />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               EDROM TEAM
